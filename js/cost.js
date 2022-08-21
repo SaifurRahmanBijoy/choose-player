6a14df0d8aa74a0b8e9d960562e9bdd59ff0ecbf
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-08-22 03:13:37 +0600

    responsive and functional

--- js/cost.js
+++ js/cost.js
@@ -1,7 +1,17 @@
 document.getElementById("btn-calculate").addEventListener("click", function () {
   const perPlayerInput = document.getElementById("per-player-input");
   const perPlayerInputvalueString = perPlayerInput.value;
-  const perPlayerInputvalue = parseFloat(perPlayerInputvalueString);
+  if (document.getElementById("name-list").childElementCount < 4) {
+    alert("You've not selected 5 Players!");
+    return;
+  } else if (
+    isNaN(perPlayerInputvalueString) ||
+    perPlayerInputvalueString == ""
+  ) {
+    alert("please enter amount correctly!");
+    return;
+  }
+  const perPlayerInputvalue = parseInt(perPlayerInputvalueString);
   const playerExpenses = document.getElementById("player-expenses");
   playerExpenses.innerText = perPlayerInputvalue * 5;
 });
@@ -11,10 +21,19 @@ document
   .addEventListener("click", function () {
     const managerExpensesValue = document.getElementById("manager-expenses");
     const managerExpensesString = managerExpensesValue.value;
+    if (document.getElementById("name-list").childElementCount < 4) {
+      alert("You've not selected 5 Players!");
+      return;
+    } else if (isNaN(managerExpensesString) || managerExpensesString == "") {
+      alert("please enter amount correctly!");
+    }
     const managerExpenses = parseFloat(managerExpensesString);
 
     const coachExpensesValue = document.getElementById("coach-expenses");
     const coachExpensesString = coachExpensesValue.value;
+    if (isNaN(coachExpensesString) || coachExpensesString == "") {
+      alert("please enter amount correctly!");
+    }
     const coachExpenses = parseFloat(coachExpensesString);
 
     const perPlayerInput = document.getElementById("per-player-input");
@@ -22,5 +41,6 @@ document
     const perPlayerInputvalue = parseFloat(perPlayerInputvalueString);
 
     const totalValue = document.getElementById("total");
-    totalValue.innerText = coachExpenses + managerExpenses + (perPlayerInputvalue*5);
+    totalValue.innerText =
+      coachExpenses + managerExpenses + perPlayerInputvalue * 5;
   });
--- js/player.js
+++ js/player.js
@@ -1,11 +1,16 @@
+let count = 0;
 function playerNameInclude(playerId) {
   const playerNameText = document.getElementById(playerId);
   const playerName = playerNameText.innerText;
   const nameList = document.getElementById("name-list");
   const li = document.createElement("li");
-  li.innerText = playerName;
+  count++;
+  li.innerText = count + ". " + playerName;
   nameList.appendChild(li);
-  
+  if (document.getElementById("name-list").childElementCount >= 5) {
+    alert("Wow!You've selected 5 Players!");
+    return false;
+  }
 }
 
 document.getElementById("btn-messi").addEventListener("click", function () {
